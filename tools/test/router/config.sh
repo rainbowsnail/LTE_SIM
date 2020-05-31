4ec8d391b5c1b9fbb276bd5bcef9b7b2e53d3838
Jing <763118287@qq.com>
2020-05-31 06:33:51 +0000

    update router config

--- tools/test/router/config.sh
+++ tools/test/router/config.sh
@@ -9,5 +9,11 @@ ethtool --offload ens8 gso off
 ethtool --offload ens8 gro off
 ethtool --offload ens7 gro off
 ethtool --offload ens7 gso off
-ifconfig ens8 mtu 1400
-ifconfig ens7 mtu 1400
+#ifconfig ens8 mtu 1400
+#ifconfig ens7 mtu 1400
+#route del -net 10.4.112.0 netmask 255.255.240.0 gw 0.0.0.0
+#route del -net 10.4.96.0 netmask 255.255.240.0 gw 0.0.0.0
+route -n
+iptables  -I   INPUT   -p   all   -s 10.4.112.4   -j   DROP
+iptables  -I   INPUT   -p   all   -s 10.4.96.4   -j   DROP
+iptables -L
